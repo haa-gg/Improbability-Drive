526c4b5d3b8b5e2dcbc614506f6fd7a1bc6a3b44
Glendon Gengel <glendon.gengel@gmail.com>
2023-05-16 15:00:11 -0500

    adding offline cache

--- sw.js
+++ sw.js
@@ -23,3 +23,17 @@ var URLS = [
   `${GHPATH}/marvinvisions-bold-webfont.woff`,
   `${GHPATH}/marvinvisions-bold-webfont.woff2`,
 ]
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(async function() {
+       try{
+         var res = await fetch(event.request);
+         var cache = await caches.open('cache');
+         cache.put(event.request.url, res.clone());
+         return res;
+       }
+       catch(error){
+         return caches.match(event.request);
+        }
+      }());
+  });
