99a4ba5a74290c36dedea46e893e68cc7f13be99
Glendon Gengel <glendon.gengel@gmail.com>
2023-05-16 14:23:15 -0500

    Adding PWA capability

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Change this to your repository name
+var GHPATH = '/Improbability-Drive';
+
+// Choose a different app prefix name
+var APP_PREFIX = 'impd_';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/style.css`,
+  `${GHPATH}/custom.js`,
+  `${GHPATH}/table.json`,
+  `${GHPATH}/images/bg.jpeg`,
+   `${GHPATH}/images/icon-web.png`,
+  `${GHPATH}/marvinvisions-bold-webfont.woff`,
+  `${GHPATH}/marvinvisions-bold-webfont.woff2`,
+]
